264fc9ca429667516064a441889f77304388f4f3
Brice <brice.berdah@outlook.com>
2017-11-10 14:51:20 +0100

    second commit

---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+import json
+import requests
+
+app = Flask(__name__)
+port = '5000'
+
+@app.route('/', methods=['POST'])
+def index():
+  data = json.loads(request.get_data())
+
+  # FETCH THE CRYPTO NAME AND REMOVE ITS STRINGS
+  crypto_name = data['nlp']['entities']['crypto_name'][0]['raw']
+
+  # FETCH BTC/USD/EUR PRICES
+  r = requests.get("https://min-api.cryptocompare.com/data/price?fsym="+crypto_name+"&tsyms=BTC,USD,EUR")
+
+  return jsonify(
+    status=200,
+    replies=[{
+      'type': 'text',
+      'content': 'The price of %s is %f BTC and %f USD' % (crypto_name, r.json()['BTC'], r.json()['USD'])
+    }],
+    conversation={
+      'memory': { 'key': 'value' }
+    }
+  )
+
+@app.route('/errors', methods=['POST'])
+def errors():
+  print(json.loads(request.get_data()))
+  return jsonify(status=200)
+
+app.run(port=port)

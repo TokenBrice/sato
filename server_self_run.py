774aa2462f00747d8217b84fc726954b255470f1
Ahirice <brice.berdah@outlook.com>
2017-11-22 15:31:45 +0100

    Update server_self_run.py

--- server_self_run.py
+++ server_self_run.py
@@ -10,7 +10,7 @@ def index():
   data = json.loads(request.get_data())
 
   # FETCH THE CRYPTO NAME
-  crypto_name = data['nlp']['entities']['crypto_name'][0]['raw']
+  crypto_name = data['conversation']['memory']['crypto']['value']
   
   # FETCH BTC/USD/EUR PRICES
   r = requests.get("https://min-api.cryptocompare.com/data/price?fsym="+crypto_name+"&tsyms=BTC,USD,EUR")

584b83594cd12897b2b11dfa8f952436d5a2d57e
Ahirice <brice.berdah@outlook.com>
2017-11-22 12:15:21 +0100

    Update server.py

--- server.py
+++ server.py
@@ -13,7 +13,8 @@ def index():
   data = json.loads(request.get_data())
 
   # FETCH THE CRYPTO NAME AND REMOVE ITS STRINGS
-  crypto_name = data['nlp']['entities']['crypto_name'][0]['raw']
+  ## crypto_name = data['nlp']['entities']['crypto_name'][0]['raw']
+  crypto_name = data['conversation']['memory'][crypto][0]['raw']
 
   # FETCH BTC/USD/EUR PRICES
   r = requests.get("https://min-api.cryptocompare.com/data/price?fsym="+crypto_name+"&tsyms=BTC,USD,EUR")

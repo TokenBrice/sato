d0eb091ae617cf398d3e6addc7f7dfd546981032
Brice BERDAH <brice.berdah@outlook.com>
2019-01-07 16:37:11 +0100

    Update server.py

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ def index():
   data = json.loads(request.get_data().decode('utf-8'))
 
   # FETCH THE CRYPTO NAME
-  crypto_name = data[“nlp”][“entities”][“crypto_name”][0][“raw”]
+  crypto_name = data["nlp"]["entities"]["crypto_name"][0]["raw"]
 
   # FETCH BTC/USD/EUR PRICES
   r = requests.get("https://min-api.cryptocompare.com/data/price?fsym="+crypto_name+"&tsyms=BTC,USD,EUR")
